7420ff4c02b21840a9934eeb399db5f3cf112d02
Ethan Crawford <ethcrawford@gmail.com>
2018-09-19 09:59:27 +0300

    Add compilation order

--- brunch-config.js
+++ brunch-config.js
@@ -4,12 +4,30 @@ exports.files = {
     joinTo: {
       "js/app.js": /^app/,
       "js/vendor.js": /^vendor/
+    },
+    // Define compilation order (optional). The default order is alphabetical.
+    // App and vendor source will be placed in different files.
+    order: {
+      before: [
+        // "vendor/second.js"
+      ],
+      after: [
+        // "vendor/first.js"
+      ]
     }
   },
   stylesheets: {
     joinTo: {
       "css/app.css": /^app/,
       "css/vendor.css": /^vendor/
+    },
+    order: {
+      before: [
+        // "vendor/third.css"
+      ],
+      after: [
+        // "app/first.sass"
+      ]
     }
   }
 };

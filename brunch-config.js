287f20d356c9fac826c2890de34ec9f701429d2a
Ethan Crawford <ethcrawford@gmail.com>
2018-11-05 16:00:22 +0300

    Add autoprefixer

--- brunch-config.js
+++ brunch-config.js
@@ -46,6 +46,23 @@ exports.npm = {
   enabled: false,
 };
 
+exports.plugins = {
+  postcss: {
+    processors: [
+      require('postcss-flexbugs-fixes')(),
+      require('autoprefixer')({
+        browsers: [
+          '> 1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9',
+        ],
+        flexbox: 'no-2009',
+      }),
+    ],
+  },
+};
+
 exports.conventions = {
   ignored: [
     /\/_/,
